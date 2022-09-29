3c465fc384eb4c1e90f06a84dfff99d5b4ccf30d
Liuyuan <2787141886@qq.com>
2022-09-29 15:56:42 +0800

    finished ver 4

--- Ver_4/client.c
+++ Ver_4/client.c
@@ -11,11 +11,11 @@
 #include <sys/stat.h>
 #include <arpa/inet.h>
 
-#define MAX_SIZE 1024
-#define BUF_SIZE 100
-#define HED_SIZE 100
-#define TOL_SIZE BUF_SIZE+HED_SIZE
-#define PLACE_HOLDER 1
+#define MAX_SIZE 1024					// the max size of buffer to restore file name
+#define BUF_SIZE 100					// the size of package data buffer
+#define HED_SIZE 100					// the size of package header buffer
+#define TOL_SIZE BUF_SIZE+HED_SIZE		// the total size of package
+#define PLACE_HOLDER 1					// empty character fill
 
 void error_handling(char* message);
 void service(int serv_fd);
@@ -23,22 +23,21 @@ void send_file(int serv_fd);
 
 int main(int argc, char *argv[])
 {
-    // 程序参数检查
     if (argc != 3)
     {
-        printf("Using:./client ip port\nExample:./client 127.0.0.1 5005\n\n");
+        printf("Usage:./client ip port\nExample:./client 127.0.0.1 5005\n\n");
         return -1;
     }
 
-    // 创建一个Socket
+	// create socket
     int sockfd;
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
     {
-        perror("socket");
+        error_handling("socket");
         return -1;
     }
 
-    // 向服务器发起连接请求
+	// obtain the IP address based on the domain name
     struct hostent *h;
     if ((h = gethostbyname(argv[1])) == 0) // 指定服务端的ip地址
     {
@@ -46,18 +45,23 @@ int main(int argc, char *argv[])
         close(sockfd);
         return -1;
     }
+
+	// set the information of server
     struct sockaddr_in servaddr;
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(atoi(argv[2])); // 指定服务端的通信端口
+    servaddr.sin_port = htons(atoi(argv[2])); 
     memcpy(&servaddr.sin_addr, h->h_addr, h->h_length);
+
+	// connect server
     if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) != 0) // 向服务端发起连接请求
     {
-        perror("connect");
+        error_handling("connect");
         close(sockfd);
         return -1;
     }
 
+	// let service function handle the connection
 	service(sockfd);
 
     // 关闭socket
@@ -75,9 +79,11 @@ void error_handling(char* message)
 
 void service(int serv_fd)
 {
+	// get a new process
 	pid_t pid = fork();	
 
 	if (pid == -1) { error_handling("fork() error"); }
+	// send message
 	else if (pid == 0)
 	{
 		char buffer[1024];
@@ -86,20 +92,23 @@ void service(int serv_fd)
 		{
 			int iret;
 			
-			// send message
 			memset(buffer, 0, sizeof(buffer));
 			printf(": ");
+			// get message from stdin
 			fgets(buffer, sizeof(buffer) - 1, stdin);
+			// send file
 			if (strcmp("file\n", buffer) == 0)
 			{
 				send_file(serv_fd);
 			}
+			// send message
 			else if ((iret = send(serv_fd, buffer, strlen(buffer), 0)) <= 0)
 			{
 				printf("iret = %d\n", iret);
 			}
 		}
 	}
+	// receive data from server
 	else
 	{
 		char buffer[1024];
@@ -123,16 +132,19 @@ void service(int serv_fd)
 void send_file(int serv_fd)
 {
 	int iret;
+	// set the buffer for file name and package
 	char file_name[MAX_SIZE], buffer[BUF_SIZE + HED_SIZE];
 	FILE *file_ptr = NULL;
 	FILE *log_ptr = NULL; 
 
 	// initialize log_ptr
+	// generate name of log file
 	char log_name[MAX_SIZE];
 	time_t cur_time = time(NULL);
 	strcpy(log_name, "logs/log_");	
 	strncat(log_name, ctime(&cur_time) + 11, 8);
 	strcat(log_name, ".txt");
+	// create log file
 	log_ptr = fopen(log_name, "w");	
 
 	// get file name
@@ -140,39 +152,50 @@ void send_file(int serv_fd)
 	scanf("%s", file_name);
 	file_ptr = fopen(file_name, "r");
 
+	// if faile to open file
 	if (!file_ptr)
 	{
 		printf("fopen() failed\n");
 		printf("errno = %d, reason = %s\n", errno, strerror(errno));
-		system("pause");
+		// system("pause");
 	}
 	else 
 	{
 		printf("file %s opened\n", file_name);
+		// record package number
 		size_t count = 0;
+		// record how many bytes will be sent this time
 		size_t sent_size = 0;
 
 		// get the total size of file
+		// file_stat to restore the info of file
 		struct stat file_stat;
 		stat(file_name, &file_stat);	
+		// get the size of file
 		const size_t file_size = file_stat.st_size;
 
 		int flag = 1;
 		while (flag)
 		{
-			memset(buffer, PLACE_HOLDER, sizeof(buffer));
+			// initialize buffer
+			memset(buffer, PLACE_HOLDER, HED_SIZE);
+			memset(buffer + HED_SIZE, 0, BUF_SIZE);
 
 			// write header
 			count++;
 			int offset = sprintf(buffer, "FILE: %s - package %zu of total %zu:\n", file_name, count, (file_size % BUF_SIZE) == 0 ? (file_size / BUF_SIZE) : (file_size / BUF_SIZE + 1));	
-			// write 0 into space
+			// replace the \0 character at the end of header
 			buffer[offset] = PLACE_HOLDER;
 
-			sent_size = fread(buffer + HED_SIZE, sizeof(char), BUF_SIZE - 1, file_ptr);
+			sent_size = fread(buffer + HED_SIZE, sizeof(char), BUF_SIZE, file_ptr);
+			// if there is still package to send
 			if (sent_size > 0)
 			{
+				// write log
 				fprintf(log_ptr, "buffer:\n%s\n", buffer);
 				fprintf(log_ptr, "===============================\n");
+
+				// send package
 				if ((iret = send(serv_fd, buffer, TOL_SIZE, 0)) <= 0)
 				{
 					printf("iret = %d\n", iret);
@@ -180,7 +203,8 @@ void send_file(int serv_fd)
 				}
 				printf("FILE: %s - package %-5zu sent\n",file_name, count);
 #ifdef SLEEP
-	usleep(100000);
+				// sleep a while so that the server can receive the next package
+				usleep(100000);
 #endif
 			}
 			else
--- Ver_4/server.c
+++ Ver_4/server.c
@@ -8,22 +8,28 @@
 #include <sys/socket.h>
 #include <sys/stat.h>
 
-#define BUF_SIZE	1024
-#define HED_SIZE	100
+#define BUF_SIZE	1024	// size of buffer to receive data
+#define HED_SIZE	100		// size of header in data
 #define RECEIVED		1
 #define NOT_RECEIVED	0
 
-char receiving = 0;
-time_t last_packge = 0;
-char package_status[65536];
-size_t package_total_num;
+char receiving = 0;						// if the server is receiving file
+char package_status[65536];				// records whether the package corresponding to the index is received
+char package_status_initialized = 0;	// record if the package_status array is initialized
+size_t package_total_num;				// the total number of packages
 
 void error_handling(char* message);
+
+// service function calls my_send & my_recv
 void service(const char* ip, int clnt_fd);
+// send message to client
 void my_send(const char* ip, int clnt_fd);
+// receive message from client
 void my_recv(const char* ip, int clnt_fd);
+// save the file from client
 void save_file(const char* buffer, size_t size, int clnt_fd);
-void call_resume(char* status, size_t length, size_t message_count, int clnt_fd);
+// notify the client of package loss
+void call_resume(char* status, size_t message_count, int clnt_fd);
 
 int main(int argc, char* argv[])
 {
@@ -32,32 +38,38 @@ int main(int argc, char* argv[])
 		fprintf(stdout, "Usage : %s <port>", argv[0]);
 	}
 
+	// listen socket
 	int listen_fd;
-	if ((listen_fd = socket(PF_INET, SOCK_STREAM, 0)) == -1)
-	{ error_handling("socket() error"); }
+	if ((listen_fd = socket(PF_INET, SOCK_STREAM, 0)) == -1) { error_handling("socket() error"); }
 
+	// set server protocal 
 	struct sockaddr_in serv_addr;
 	memset(&serv_addr, 0, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	serv_addr.sin_port = htons(atoi(argv[1]));
 
+	// save the info of client
 	struct sockaddr_in clnt_addr;
 	socklen_t clnt_addr_sz = sizeof(clnt_addr);
 
+	// bind listen socket and server struct
 	if (bind(listen_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
 	{ error_handling("bind() error"); }
 
+	// start listening
 	if (listen(listen_fd, 5) == -1)
 	{ error_handling("listen() error"); }
 
 	pid_t pid;
 	while (1)
 	{
+		// accept connection and create socket with client
 		int clnt_fd;
 		if ((clnt_fd = accept(listen_fd, (struct sockaddr *)&clnt_addr, &clnt_addr_sz)) == -1)
 		{ error_handling("accept() error"); }
 
+		// print log on stdout
 		fprintf(stdout, "========== Connection Acctped ==========\n");
 		fprintf(stdout, "IP: %s \n", inet_ntoa(clnt_addr.sin_addr));
 		fprintf(stdout, "port: %d \n", ntohs(clnt_addr.sin_port));
@@ -68,6 +80,7 @@ int main(int argc, char* argv[])
 		if (pid == -1) { error_handling("fork() error"); }
 		else if (pid == 0) 
 		{
+			// close listen_fd for it is only used in main process
 			close(listen_fd);
 			service(inet_ntoa(clnt_addr.sin_addr), clnt_fd);
 			exit(0);
@@ -92,6 +105,8 @@ void error_handling(char* message)
 
 void service(const char* ip, int clnt_fd)
 {
+	// create another process in the child process
+	// one for send, another for receive
 	pid_t pid = fork();
 
 	if (pid == -1) { error_handling("fork() error"); }
@@ -106,10 +121,17 @@ void my_send(const char* ip, int clnt_fd)
 	while (1)
 	{
 		int iret;
+		// set buffer
 		memset(buffer, 0, sizeof(buffer));
+		// display an input prompt
 		fprintf(stdout, "To %s : ", ip);
+		// get message from stdin
 		fgets(buffer, sizeof(buffer) - 1, stdin);
+
+		// close connection
 		if (strcmp(buffer, "quit\n") == 0) { close(clnt_fd); break; } 
+
+		// if connection is broken
 		if ((iret = send(clnt_fd, buffer, strlen(buffer), 0)) <= 0)
 		{
 			fprintf(stdout, "iret = %d \n", iret);
@@ -126,13 +148,16 @@ void my_recv(const char* ip, int clnt_fd)
 	while (1)
 	{
 		// breakpoint resume
+		// if is receiving file, check if there are lost packages
 		if (receiving)	
 		{
-			call_resume(package_status, 1024, package_total_num, clnt_fd);
+			call_resume(package_status, package_total_num, clnt_fd);
+			// output message immediately
 			fflush(stdout);
 		}
 
 		int iret;
+		// if disconnected, output log
 		if ((iret = recv(clnt_fd, buffer, sizeof(buffer), 0)) <= 0 )
 		{
 			fprintf(stdout, "\n========================================\n");
@@ -140,13 +165,15 @@ void my_recv(const char* ip, int clnt_fd)
 			fprintf(stdout, "========================================\n");
 			break;
 		}
+
+		// close connection
 		if ( strcmp(buffer, "quit\n") == 0 ) { close(clnt_fd); break; }
 		// save file
 		else if (strncmp(buffer, "FILE:", 5) == 0) 
 		{
-			last_packge = time(NULL);
-
+			// set flag
 			receiving = 1;
+			// let save_file to handle the package
 			save_file(buffer, sizeof(buffer), clnt_fd); 
 			memset(buffer, 0, sizeof(buffer));
 		}
@@ -162,14 +189,13 @@ void my_recv(const char* ip, int clnt_fd)
 void save_file(const char* buffer, size_t size, int clnt_fd)
 {
 	// buffer declaration
-	char file_name[1024];	
-	char package_count_str[1024];
-	char package_total_num_str[1024];
-	static size_t package_count = 0;
-	int package_num = 0;
+	char file_name[1024];				// record the name of file	
+	char package_count_str[1024];		// buffer to restore the serial number of the package 
+	int package_num = 0;				// integer to restore the serial number of the package
+	char package_total_num_str[1024];	// buffer to restore the total number of packages
 	
 	// breakpoint resume
-	static char package_status_initialized = 0;
+	// if the status array is not initialized, initialize it
 	if (!package_status_initialized) 
 	{
 		memset(package_status, NOT_RECEIVED, sizeof(package_status));
@@ -177,12 +203,15 @@ void save_file(const char* buffer, size_t size, int clnt_fd)
 	}
 	
 	// save data
+	// extract infos from header string
 	sscanf(buffer, "FILE: %s - package %s of total %s:", file_name, package_count_str, package_total_num_str);
+	// append idenfitier after file name
 	strcat(file_name, "_server");
+	// save the data in the string to the integer
 	package_num = atoi(package_count_str);
 	package_total_num = atoi(package_total_num_str);
 	
-	// printf("filename: %s\npackage_str: %zu\n", file_name, package_count);
+	// output log
 	printf("====================\n");
 	printf("FILE NAME:\t%s\n", file_name);
 	printf("PACKAGE NUMBER:\t%d\n", package_num);
@@ -192,27 +221,35 @@ void save_file(const char* buffer, size_t size, int clnt_fd)
 	if (package_num >= 0)
 	{	
 		// save file
-		package_count ++;
 		FILE* file = fopen(file_name, "a+");
+		// move the file pointer to the end of file
 		fseek(file, 0, SEEK_END);
+		// append data
 		fprintf(file, "%s", buffer + HED_SIZE );
-		package_status[package_count] = RECEIVED;
+		// set flag
+		package_status[package_num] = RECEIVED;
 
 		fclose(file);	
 	}
+	// if package_num < 0, it means server received the last package
+	// start breakpoint resume
 	else
 	{
-		call_resume(package_status, 1024, package_total_num, clnt_fd);
+		call_resume(package_status, package_total_num, clnt_fd);
 	}
 }
 
-void call_resume(char* status, size_t length, size_t message_count, int clnt_fd)
+void call_resume(char* status, size_t message_count, int clnt_fd)
 {
 	// breakpoint resume
 	size_t ite = 0;
+	// assume all packages are received
 	char all_received = 1;
+
+
 	for (ite = 1; ite <= message_count; ite++)
 	{
+		// if the flag of a package is not set
 		if (status[ite] == NOT_RECEIVED)
 		{
 			all_received = 0;
@@ -220,6 +257,7 @@ void call_resume(char* status, size_t length, size_t message_count, int clnt_fd)
 			
 			sprintf(temp, "FILE: packag %zu NOT_RECEIVED", ite);
 			printf("FILE: packag %zu NOT_RECEIVED\n", ite);
+			// tell the client there is a package lost
 			send(clnt_fd, temp, strlen(temp), 0);
 		}
 	}
@@ -228,6 +266,9 @@ void call_resume(char* status, size_t length, size_t message_count, int clnt_fd)
 		printf("====================\n");
 		printf("ALL PACKAGES RECEIVED!\n");
 		printf("====================\n");
+		// we are not receiving packages now
 		receiving = 0;
+		// we need to re-initialize the array
+		package_status_initialized = 0;
 	}
 }
